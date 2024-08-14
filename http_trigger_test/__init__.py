d03008bdf3cd17e960a03cd8ecdc211b2db2e435
edgarmatus <ematus@bmsoluciones.cl>
2024-08-14 11:32:53 -0400

    Se modifica estructura de proyecto

--- http_trigger_test/__init__.py
+++ http_trigger_test/__init__.py
@@ -1,13 +1,12 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
-@app.route(route="http_trigger_test")
-def http_trigger_test(req: func.HttpRequest) -> func.HttpResponse:
+# @func.Function('http_trigger_test')
+async def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    
     if not name:
         try:
             req_body = req.get_json()
